f9c809da6ee15fc037d98215c6962e7e4a1af193
IamEssJay <emmasj2019@gmail.com>
2023-03-25 08:47:26 +0000

    First commit useAuuth state giving error

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+
+    const style={
+        nav:`bg-gray-800 h-20 flex justify-between items-center p-4`,
+        heading:`text-white text-3xl`
+
+    }
+
+    return (
+        <div className={style.nav}>
+            <h1 className={style.heading}> Chat App</h1>
+        </div>
+      );
+}
+ 
+export default Navbar;
